91586f31197c8636c43480eefe39fde2955e383f
Hanh Nguyen Vu <hanhnguyen.vu@aalto.fi>
2024-03-30 14:39:37 +0300

    add something so that the turns are also saved in textBased when save the game as a ext file

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,4 +1,5 @@
-
+import java.io.{BufferedWriter, File, FileWriter}
+import scala.io.Source
 import scala.collection.mutable
 
 class Game():
@@ -13,7 +14,7 @@ class Game():
   var lastCapturingPlayer: Option[Player] = None
   def setLastCapturingPlayer(player: Player): Unit =
     lastCapturingPlayer = Some(player)
-  
+
   // the rounds + turn
   def addPlayer(player: Player) =
     players += player
@@ -22,7 +23,10 @@ class Game():
     if table.isEmpty && players.size >= 2 then
       table.cardsOnTable = deck.remainings.take(4)
       deck.remainings.drop(4)
-
+      
+  var notQuittingPlayers = players.filterNot(p=>p.playerQuit)
+  players = notQuittingPlayers
+  
   def playTurn(input: String) =
     if players.size >= 2 then
       var currentPlayer = players(numTurn)
@@ -30,18 +34,13 @@ class Game():
       commands.askAction(currentPlayer)
       if commands.action != "show" && commands.action != "show pile" && isValid then
         this.deck.deal(currentPlayer)
-        if numTurn + 1 < players.size  then numTurn += 1
+        if numTurn + 1 < players.size  then
+          numTurn += 1
+          turn += 1
         else numTurn = 0
     else
       numTurn = 0
       print("Not enough players in the game. Cannot start game.")
 
-  def endGame = players.exists(p => p.score >= 16) || deck.remainings.isEmpty || !players.exists(p => p.hand.nonEmpty)
-
-  //erase the quitting player in the game
-  val quittingPlayers = players.filter(p=>p.playerQuit)
-  players --= quittingPlayers
-
-//  def save(fileName: String) =
-
-
+  def endGame = players.exists(p => p.score >= 16) || deck.remainings.isEmpty || !players.exists(p => p.hand.nonEmpty) 
+  def saved = players.exists(p => p.wantsToSave)
