78284af67ccb55016405e258ec19efc4d7bb5deb
Hanh Nguyen Vu <hanhnguyen.vu@aalto.fi>
2024-03-30 23:37:24 +0300

    add gameload

---
+++ src/main/scala/gameLoad.scala
@@ -0,0 +1,123 @@
+import scala.collection.mutable
+import scala.io.Source
+
+object gameLoad:
+  def loadGameFromFile(filename: String) =
+    val source = Source.fromFile(filename)
+    try {
+      val game = Game()
+      val content = source.mkString
+      val contentLines = content.split("\n")
+      val playersInfo = content.split("\n").dropRight(5)
+      val tableInfo = contentLines(contentLines.indexWhere(_.startsWith("Table")))
+      val deckInfo = contentLines(contentLines.indexWhere(_.startsWith("Deck")))
+      val turnIndex = contentLines.indexWhere(_.startsWith("Turns:"))
+      val lastIndex = contentLines.indexWhere(_.startsWith("Last"))
+
+      val playernames = getPlayersNames(playersInfo)
+      val playerscores = getPlayersScores(playersInfo)
+      val playerhand = getPlayerHands(playersInfo,game)
+      val playerpile = getPlayerPiles(playersInfo,game)
+      val tableCards = getTable(tableInfo,game)
+      val deckCards = getDeck(deckInfo,game)
+
+      if turnIndex != -1 then
+        val turn = contentLines(turnIndex).split(":")(1).trim.toInt
+        val lastcapturer = contentLines(lastIndex).split(":")(1).trim
+        game.turn = turn
+        game.numTurn = game.turn % playernames.size
+        for i <- playernames.indices do
+          game.addPlayer(Player(playernames(i),game))
+          Player(playernames(i),game).score = playerscores(i)
+          Player(playernames(i),game).hand = playerhand(i)
+          Player(playernames(i),game).pile = playerpile(i)
+          Player(playernames(i),game).wantsToSave = false
+        game.table.cardsOnTable = tableCards
+        game.deck.remainings = deckCards
+        
+        if playernames.contains(lastcapturer) then game.setLastCapturingPlayer(Player(lastcapturer,game))
+        game
+      else Game()
+    }
+    finally
+      source.close()
+
+  def getPlayersNames(playerLines: Array[String]): mutable.Buffer[String] =
+    val eachplayerin4 = playerLines.drop(1)
+    var playernames = mutable.Buffer[String]()
+    var i = 0
+    for i <- eachplayerin4.indices do
+      if i % 4 == 0 then
+        playernames += eachplayerin4(i).split(":")(0).trim
+    playernames
+
+  def getPlayersScores(playerLines: Array[String]): mutable.Buffer[Int] =
+    val eachplayerin4 = playerLines.drop(1)
+    var playerscores = mutable.Buffer[Int]()
+    var i = 0
+    for i <- eachplayerin4.indices do
+      if i % 4 == 0 then
+        playerscores += eachplayerin4(i).split(":")(1).trim.toInt
+    playerscores
+
+  def getPlayerHands(playerLines: Array[String],game:Game) =
+    val eachplayerin4 = playerLines.drop(1)
+    var playerhands = mutable.Buffer[String]()
+    var buffer = mutable.Buffer[mutable.Buffer[Cards]]()
+    var i = 0
+    for i <- eachplayerin4.indices do
+      if i % 4 == 1 then
+        playerhands += eachplayerin4(i).split(":")(1).trim
+
+    for p <- playerhands do
+      if p.split(" of ").length == 2 then
+        var pname = p.split(" of ")(0).trim
+        var psuit = p.split(" of ")(1).trim
+        var eachbuffer = mutable.Buffer[Cards]()
+        eachbuffer += Cards(psuit,pname,game)
+        buffer += eachbuffer
+      else buffer += mutable.Buffer[Cards]()
+    buffer
+
+  def getPlayerPiles(playerLines: Array[String],game:Game) =
+    val eachplayerin4 = playerLines.drop(1)
+    var playerpiles = mutable.Buffer[String]()
+    var buffer = mutable.Buffer[mutable.Buffer[Cards]]()
+    var i = 0
+    for i <- eachplayerin4.indices do
+      if i % 4 == 2 then
+        playerpiles += eachplayerin4(i).split(":")(1).trim
+
+    for p <- playerpiles do
+      if p.split(" of ").length == 2 then
+        var pname = p.split(" of ")(0).trim
+        var psuit = p.split(" of ")(1).trim
+        var eachbuffer = mutable.Buffer[Cards]()
+        eachbuffer += Cards(psuit,pname,game)
+        buffer += eachbuffer
+      else buffer += mutable.Buffer[Cards]()
+    buffer
+
+  def getTable(tableLines: String,game:Game) =
+    var table = tableLines.split(":")(1).trim
+    var eachcard = table.split(", ")
+    var buffer = mutable.Buffer[Cards]()
+    for card <- eachcard do
+      if card.split(" of ").length == 2 then
+        val name = card.split(" of ")(0).trim
+        val suit = card.split(" of ")(1).trim
+        buffer += Cards(suit,name,game)
+      else buffer = mutable.Buffer[Cards]()
+    buffer
+
+  def getDeck(deckLines: String,game:Game) =
+    var deck = deckLines.split(":")(1).trim
+    var eachcard = deck.split(", ")
+    var buffer = mutable.Buffer[Cards]()
+    for card <- eachcard do
+      if card.split(" of ").length == 2 then
+        val name = card.split(" of ")(0).trim
+        val suit = card.split(" of ")(1).trim
+        buffer += Cards(suit,name,game)
+      else buffer = mutable.Buffer[Cards]()
+    buffer
